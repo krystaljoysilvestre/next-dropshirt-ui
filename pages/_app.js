554bc18361aaecbe14afec16c1b37df14b25a5c9
rockysantiago <6001739+rockysantiago@users.noreply.github.com>
2022-02-27 16:00:42 +0800

    feat: add redux-toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,24 @@
 import { SSRProvider } from '@react-aria/ssr';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
+import { Layout } from 'components';
+import { store } from 'lib/store';
 import GlobalStyle from 'styles/global';
 import theme from 'styles/theme';
-
-import { Layout } from 'components';
-
 import 'antd/dist/antd.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <SSRProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </Provider>
     </SSRProvider>
   );
 }
